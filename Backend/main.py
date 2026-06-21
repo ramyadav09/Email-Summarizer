38e3884e723c7a65e9a1fa911d60011d2d4fd97d
Ram Kumar Yadav <ramyadav2060@gmail.com>
2026-06-21 19:38:48 +0530

    app ui and layout

--- Backend/main.py
+++ Backend/main.py
@@ -5,3 +5,27 @@ service = build("gmail", "v1", credentials=creds)
 results = service.users().messages().list(userId="me", maxResults=10).execute()
 
 messages = results.get("messages", [])
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+app.get("/summarize")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Tokens(BaseModel):
+    access_token: str
+    refresh_token: str
+
+
+@app.post("/login")
+def read_root(tokens: Tokens):
+    print(tokens.access_token)
+    print(tokens.refresh_token)
